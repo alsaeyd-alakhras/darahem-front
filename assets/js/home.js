a89deb2707fad3fe59346e97d8783e08c8ac97e5
Al-Saeyd J Al-Akhras (السيد الاخرسي) <79516145+alsaeyd-alakhras@users.noreply.github.com>
2026-08-22 12:02:46 +0300

    update problem & solved section

--- assets/js/home.js
+++ assets/js/home.js
@@ -192,7 +192,7 @@ $(document).ready(function () {
 
   // Stagger each grid of cards
   var gridSelectors = [
-    '.problem-solution-row',
+    '.grid.grid-cols-1.sm\\:grid-cols-2.lg\\:grid-cols-3.gap-5 > div',
     '.bg-g-light2 .grid > div',
     '#features .grid > div',
     '.bg-g-light .grid.grid-cols-1.md\\:grid-cols-3 > div'
